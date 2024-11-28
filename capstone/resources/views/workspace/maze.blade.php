a888a69290c2ffe41356f00c4836a9187a467b4b
asder2821 <dabucoljamesdanieljudilla@gmail.com>
2024-11-28 14:39:06 +0800

    added maze game

---
+++ capstone/resources/views/workspace/maze.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maze Game for Kids</title>
+    <link rel="stylesheet" href="css/stylemaze.css">
+</head>
+<body>
+    <div id="maze-container">
+        <div id="player" class="cell"></div>
+        <div id="finish" class="cell"></div>
+        <!-- Add walls -->
+        <div class="wall"></div>
+        <div class="wall"></div>
+        <!-- Add more walls as necessary -->
+    </div>
+    <div id="message"></div>
+    <button id="reset">Reset</button>
+    <script src="js/scriptmaze.js"></script>
+</body>
+</html>
