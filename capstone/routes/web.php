88c889e096564ca1b205163786bb0152386951e4
baklang naglalaptop <gianleuterio@gmail.com>
2024-12-01 12:48:39 +0800

    optimized sound game

--- capstone/routes/web.php
+++ capstone/routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return view('loggedOut.index');
-
+    return view('workspace.game');
+});
 Route::get('/loggedOut/seemore', function () {
-    return view('loggedOut/seemore');
+    return view('seemore');
 })->name('seemore');
 
 Route::get('/loggedOut/seemore', function () {
     return view('loggedOut/seemore');
-})->name('loggedOut/seemore');
+})->name('seemore');
 
