786868e0abb5816407ddbef4d346d67705b12a49
Jade <dabucoljamesdanieljudilla@gmail.com>
2024-12-14 21:13:03 +0800

    try

--- capstone/resources/views/loggedIn/chat.blade.php
+++ capstone/resources/views/loggedIn/chat.blade.php
@@ -9,8 +9,6 @@
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/chat.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="chat.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
