e18f0615001a81ebc79b08ed05b532a12c645dff
baklang naglalaptop <gianleuterio@gmail.com>
2025-01-27 01:56:16 +0800

    sa admin caht side, sort users by most recent chat pero kelangan pa irefresh

--- capstone/resources/views/admin/chats.blade.php
+++ capstone/resources/views/admin/chats.blade.php
@@ -46,24 +46,15 @@
                     <div class="list-group chat-list" id="chatList" style="max-height: 500px; overflow-y: auto;">
                         <ul class="list-group list-group-flush">
                             @if($chats->isEmpty())
-                                <!-- If no chats found, show all users -->
-                                @isset($users)
-                                    @foreach ($users as $user)
-                                        <li class="list-group-item d-flex align-items-center chat-item">
-                                            <div class="profile_info">
-                                                <span class="profile_name font-weight-bold">{{ $user->name }}</span>
-                                                <span class="id" style="display: none;">{{ $user->id }}</span>
-                                            </div>
-                                        </li>
-                                    @endforeach
-                                @endisset
+                               
+                                <li class="list-group-item">No chats available</li>
                             @else
                                 <!-- If chats are found, display chat profiles -->
                                 @foreach ($chats as $chat)
                                     <li class="list-group-item d-flex align-items-center chat-item">
                                         @if ($chat->sender_id == Auth::guard('admin')->id())
                                             @if ($chat->receiver)
-                                                <img src="{{ asset('storage/' . $chat->receiver->picture) }}" class="profile_img rounded-circle mr-3" style="width: 40px; height: 40px;" alt="Profile Picture">
+                                                {{-- <img src="{{ asset('storage/' . $chat->receiver->picture) }}" class="profile_img rounded-circle mr-3" style="width: 40px; height: 40px;" alt="Profile Picture"> --}}
                                                 <div class="profile_info">
                                                     <span class="profile_name font-weight-bold">{{ $chat->receiver->name }}</span>
                                                 </div>
@@ -74,7 +65,7 @@
                                             @endif
                                         @else
                                             @if ($chat->sender)
-                                                <img src="{{ asset('storage/' . $chat->sender->picture) }}" class="profile_img rounded-circle mr-3" style="width: 40px; height: 40px;" alt="Profile Picture">
+                                                {{-- <img src="{{ asset('storage/' . $chat->sender->picture) }}" class="profile_img rounded-circle mr-3" style="width: 40px; height: 40px;" alt="Profile Picture"> --}}
                                                 <div class="profile_info">
                                                     <span class="profile_name font-weight-bold">{{ $chat->sender->name }}</span>
                                                 </div>
