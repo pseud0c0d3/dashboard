2de04cafa4a43fa674d6f9a05735d1c5ed7978a3
baklang naglalaptop <gianleuterio@gmail.com>
2025-01-31 00:43:27 +0800

    disabled comments for admin posts

--- capstone/resources/views/posts/showadmin.blade.php
+++ capstone/resources/views/posts/showadmin.blade.php
@@ -117,7 +117,15 @@
 
         <!-- Display Comments -->
         @if($post->comments->isEmpty())
-            <p style="font-family: 'Roboto', sans-serif; font-size: 0.9rem; color: rgb(102, 102, 102);">No comments yet. Be the first to comment!</p>
+            @if($post->admin_id === 1)
+                <p style="font-family: 'Roboto', sans-serif; font-size: 0.9rem; color: rgb(102, 102, 102);">
+                    Comments are disabled for administrator posts.
+                </p>
+            @else
+                <p style="font-family: 'Roboto', sans-serif; font-size: 0.9rem; color: rgb(102, 102, 102);">
+                    No comments yet. Be the first to comment!
+                </p>
+        @endif
         @else
         @foreach($post->comments as $comment)
         <div class="card mb-2" style="box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;">
